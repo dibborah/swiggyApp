abc9173c4d8418d5dd0bd31e990767f485450307
dibborah <90625406+dibborah@users.noreply.github.com>
2023-09-27 19:34:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './header/index';
+
+function App() {
+  return (
+    <div className="app">
+       <Header/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./style.css"
+
+export default function index() {
+  return (
+    <>
+    <div className="nav-bar">
+      <div>
+          <h1 className='logo'>Swiggy</h1>
+      </div>
+      <div className="btn">
+          <button className="btn1">Login</button>
+          <button className="btn2">Signup</button>        
+      </div>
+
+    </div>
+    </>
+  )
+}
